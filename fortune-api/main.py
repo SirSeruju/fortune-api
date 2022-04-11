e752906d6855798b131170886bd08dcdd201613b
Seruju <sir.seruju@yandex.ru>
2022-04-12 02:13:15 +0300

    Add CORS

--- fortune-api/main.py
+++ fortune-api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
 
@@ -9,6 +10,13 @@ setup_logging()
 app = FastAPI(
     openapi_url="/api/openapi.json",
     docs_url="/api/docs"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
